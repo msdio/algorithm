8690ba0cf0fc2db35b66793789c808991d512063
msdio <dev.msdio@gmail.com>
2024-01-20 09:53:44 +0900

    bj 2003, 3273

---
+++ haeram/2003.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+"""
+1. 완탐으로 하면 10^8 이긴 한데... 시간이 0.5초다. 완탐은 안됨.
+2. 정렬한 다음
+3. 두 번째 for문에서 시작 점을 i로 설정하고, 합이 넘거나 같은 경우 break 하면 되지 않을까?
+"""
+
+n, m = map(int, stdin.readline().split())
+arr = list(map(int, stdin.readline().split()))
+
+arr.sort()
+
+ans = 0
+for i in range(n):
+    cur_sum = 0
+
+    for j in range(i, n):
+        if cur_sum + arr[j] > m:
+            break
+
+        if cur_sum + arr[j] == m:
+            ans += 1
+            break
+
+        cur_sum += arr[j]
+
+print(ans)
