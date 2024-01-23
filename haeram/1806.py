e1e22cc8cf423ca831a5cd2f22d70d03429d324e
msdio <dev.msdio@gmail.com>
2024-01-23 21:08:07 +0900

    solving 1806

---
+++ haeram/1806.py
@@ -0,0 +1,46 @@
+from sys import stdin
+
+"""
+start, end 둘 다 0에서 시작한다.
+만약 합이 s보다 작다면 end를 뒤로 민다.
+s보다 크다면 start를 앞으로 민다.
+
+s와 같다면 현재 길이를 ans에 업데이트한다.
+
+이렇게 하면 시간복잡도가 O(2n)이다.
+"""
+
+n, s = map(int, stdin.readline().split())
+arr = list(map(int, stdin.readline().split()))
+
+start = 0
+end = 0
+
+ans = 10**6 + 1
+partial_sum = 2 * arr[0]
+while end < n:
+    if partial_sum == s:
+        ans = end - start + 1
+
+        # update index
+        partial_sum -= arr[start]
+        start += 1
+        end += 1
+        partial_sum += arr[end]
+
+        continue
+
+    if partial_sum < s:
+        end += 1
+        partial_sum += arr[end]
+        continue
+
+    if partial_sum > s:
+        partial_sum -= arr[start]
+        start += 1
+
+
+if ans == 10**6 + 1:
+    print(0)
+else:
+    print(ans)
